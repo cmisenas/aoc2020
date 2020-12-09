6af407ec5006a8ce8a46e23ec38b57a6505fe065
Jade Misenas <cjmisenas@gmail.com>
2020-12-08 22:58:54 -0800

    Write better code

--- src/day9.rs
+++ src/day9.rs
@@ -4,64 +4,46 @@ use std::path::Path;
 
 pub fn main() {
     let lines = read_lines_as_int("./day9.input");
-    let mut answer1 = 0;
     let preamble = 25;
-    let mut answer2 = 0;
-    answer1 = solve1(preamble, lines.clone());
-    answer2 = solve2(answer1, lines.clone());
+    let answer1 = solve1(preamble, &lines);
+    let answer2 = solve2(answer1, &lines);
 
     println!("Answer 1 {}", answer1);
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(preamble: usize, nums: Vec<i64>) -> i64 {
-    let mut prev_nums: Vec<i64> = Vec::new();
-    let mut start = 0;
-    let mut index: usize = preamble;
-    let mut curr_num = nums[index];
-    prev_nums.extend(nums[start..index].iter().cloned());
-    while is_valid(curr_num, prev_nums.clone()) && index < nums.len() - 1 {
-        prev_nums.push(nums[index]);
-        prev_nums.remove(0);
-        index += 1;
-        start += 1;
-        curr_num = nums[index];
-    }
-    curr_num
+fn solve1(preamble: usize, nums: &Vec<i64>) -> i64 {
+    let (i, _) = nums[..nums.len() - preamble]
+        .iter()
+        .enumerate()
+        .find(|(i, _)| !is_valid(nums[preamble + i], nums[*i..*i + preamble].to_vec()))
+        .expect("No answer yielded");
+    nums[i + preamble]
 }
 
 fn is_valid(num: i64, nums: Vec<i64>) -> bool {
-    for (i, x) in nums[..nums.len() - 1].iter().enumerate() {
-        for y in nums[i + 1..].iter() {
-            if x + y == num {
-                return true;
-            }
-        }
-    }
-    false
+    nums[..nums.len() - 1]
+        .iter()
+        .enumerate()
+        .any(|(i, x)| nums[i + 1..].iter().any(|y| x + y == num))
 }
 
-fn solve2(num: i64, nums: Vec<i64>) -> i64 {
+fn solve2(num: i64, nums: &Vec<i64>) -> i64 {
     let mut size = 2;
-    let mut start = 0;
-    let mut range: Vec<i64> = Vec::new();
-    range.extend(nums[start..start + size].iter().cloned());
-    let mut not_found = true;
-    while not_found {
-        let sum = range.iter().sum::<i64>();
-        if sum == num {
-            not_found = false;
+    let mut index = 0;
+    while size < nums.len() - 1 {
+        let result = nums[..nums.len() - size]
+            .iter()
+            .enumerate()
+            .find(|(i, _)| nums[*i..*i + size].iter().sum::<i64>() == num);
+        if let Some((i, _)) = result {
+            index = i;
+            break;
         } else {
-            if (start + 1 + size) >= nums.len() {
-                start = 0;
-                size += 1;
-            } else {
-                start += 1;
-            }
-            range.clear();
-            range.extend(nums[start..start + size].iter().cloned());
+            size += 1;
         }
     }
+    let mut range: Vec<i64> = nums[index..index + size].to_vec();
     range.sort();
     range[0] + range[range.len() - 1]
 }
