ee00d7e1ee45e73905bbed3cb48c85ec258f1c39
Jade Misenas <cjmisenas@gmail.com>
2020-12-10 07:43:26 -0800

    More cleanup

--- src/day6.rs
+++ src/day6.rs
@@ -1,11 +1,15 @@
+extern crate itertools;
+
+use self::itertools::Itertools;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashSet;
-use std::collections::HashMap;
 
 pub fn main() {
     let lines = read_lines_as_str("./day6.input");
+    /* Old answer
     let mut sum_1 = 0;
     let mut sum_2 = 0;
     let mut curr_group = Vec::new();
@@ -20,27 +24,38 @@ pub fn main() {
             curr_group.push(line);
         }
     }
-    println!("Sum 1 {}", sum_1);
-    println!("Sum 2 {}", sum_2);
+    */
+    let (sum_1, sum_2) = &lines
+        .into_iter()
+        .group_by(|line| line != "")
+        .into_iter()
+        .filter_map(|(not_empty, answers)| {
+            if not_empty {
+                Some(calc_group(answers.collect::<Vec<String>>()))
+            } else {
+                None
+            }
+        })
+        .fold((0, 0), |(acc1, acc2), (a1, a2)| (acc1 + a1, acc2 + a2));
+    println!("Answers 1 {}", sum_1);
+    println!("Answers 2 {}", sum_2);
 }
 
-fn calc_group(group: &Vec<String>) -> Vec<usize> {
+fn calc_group(group: Vec<String>) -> (usize, usize) {
     let mut answers = HashSet::new();
     let mut answers_count = HashMap::new();
-    for a in group {
+    for a in group.iter() {
         for b in a.chars() {
             let c = answers_count.entry(b).or_insert(0);
             *c += 1;
             answers.insert(b);
         }
     }
-    let mut c = 0;
-    for (_, ac) in answers_count.iter() {
-        if *ac == group.len() {
-            c += 1;
-        }
-    }
-    vec![answers.len(), c]
+    let c = answers_count
+        .iter()
+        .filter(|(_, ac)| **ac == group.len())
+        .count();
+    (answers.len(), c)
 }
 
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
