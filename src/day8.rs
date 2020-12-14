7d7ab437bf47899b861c5648cbb7324a629752da
Jade Misenas <cjmisenas@gmail.com>
2020-12-13 16:42:00 -0800

    Cleanup day8

--- src/day8.rs
+++ src/day8.rs
@@ -1,51 +1,37 @@
-use std::collections::HashMap;
-/**
-extern crate regex;
-
-use self::regex::Regex;
-*/
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-enum Instruction {
-    Noop,
-    Jmp,
-    Acc,
-}
+const NOP: &str = "nop";
+const JMP: &str = "jmp";
+const ACC: &str = "acc";
 
 pub fn main() {
     let lines = read_lines_as_str("./day8.input");
-    let mut answer1 = 0;
-    let mut answer2 = 0;
     let mut ins_set: Vec<(String, i32)> = Vec::new();
-
     for line in lines {
         ins_set.push(parse_ins(line));
     }
-    answer1 = solve1(ins_set.clone());
-    answer2 = solve2(ins_set.clone());
+    let answer1 = solve1(&ins_set);
+    let answer2 = solve2(&ins_set);
 
     println!("Answer 1 {}", answer1);
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(ins_set: Vec<(String, i32)>) -> i32 {
+fn solve1(ins_set: &Vec<(String, i32)>) -> i32 {
     let mut acc_n: i32 = 0;
     let mut visited: HashSet<i32> = HashSet::new();
-    let nop = "nop";
-    let jmp = "jmp";
-    let acc = "acc";
     let mut x: i32 = 0;
     let mut no_loop = true;
     while no_loop {
         if !visited.contains(&x) {
             let (curr_ins, curr_n) = &ins_set[x as usize];
             visited.insert(x);
-            if curr_ins == jmp {
+            if curr_ins == JMP {
                 x += curr_n;
-            } else if curr_ins == acc {
+            } else if curr_ins == ACC {
                 acc_n += curr_n;
                 x += 1;
             } else {
@@ -58,52 +44,44 @@ fn solve1(ins_set: Vec<(String, i32)>) -> i32 {
     acc_n
 }
 
-fn solve2(ins_set: Vec<(String, i32)>) -> i32 {
-    let mut a: i32 = 0;
-    let nop = "nop";
-    let jmp = "jmp";
-    let acc = "acc";
+fn solve2(ins_set: &Vec<(String, i32)>) -> i32 {
+    let mut ans: i32 = 0;
     for (i, (curr_ins, curr_n)) in ins_set.iter().enumerate() {
-        let mut ans = 0;
-        if curr_ins == jmp {
+        if curr_ins == JMP {
             // Change current ins to nop
             let mut clone_ins = ins_set.clone();
-            clone_ins[i] = (nop.to_string(), *curr_n);
+            clone_ins[i] = (NOP.to_string(), *curr_n);
             ans = check_terminating(clone_ins);
-        } else if curr_ins == nop {
+        } else if curr_ins == NOP {
             // Change current ins to jmp
             let mut clone_ins = ins_set.clone();
-            clone_ins[i] = (jmp.to_string(), *curr_n);
+            clone_ins[i] = (JMP.to_string(), *curr_n);
             ans = check_terminating(clone_ins);
         }
         if ans != 0 {
-            a = ans;
             break;
         }
     }
-    a
+    ans
 }
 
 // If it terminates, it returns a non-zero integer
 fn check_terminating(ins_set: Vec<(String, i32)>) -> i32 {
     let mut acc_n: i32 = 0;
     let mut visited: HashSet<i32> = HashSet::new();
-    let nop = "nop";
-    let jmp = "jmp";
-    let acc = "acc";
     let mut x: i32 = 0;
     let mut stop_loop = false;
     let mut no_loop = true;
     let limit = ins_set.len() as i32;
     while !stop_loop {
-        if x == limit {
+        if x >= limit {
             stop_loop = true;
         } else if !visited.contains(&x) && x < limit {
             let (curr_ins, curr_n) = &ins_set[x as usize];
             visited.insert(x);
-            if curr_ins == jmp {
+            if curr_ins == JMP {
                 x += curr_n;
-            } else if curr_ins == acc {
+            } else if curr_ins == ACC {
                 acc_n += curr_n;
                 x += 1;
             } else {
